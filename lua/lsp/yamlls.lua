a5619acb419b7df1e9e68647dec9459336d3b664
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2026-01-09 19:31:59 +0900

    mkdocs.ymlのスキーマを追加

--- lua/lsp/yamlls.lua
+++ lua/lsp/yamlls.lua
@@ -17,6 +17,10 @@ return {
         url = "https://www.schemastore.org/api/json/catalog.json",
         enable = true,
       },
+      schemas = {
+        ["https://squidfunk.github.io/mkdocs-material/schema.json"] = "mkdocs.yml",
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+      },
       customTags = {
         "tag:yaml.org,2002:python/name:pymdownx.superfences.fence_code_format",
         "tag:yaml.org,2002:python/name:material.extensions.emoji.twemoji",
