a8ea1af76add6a08caed276aa78a70bc1fa4c42e
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2026-02-02 10:12:03 +0900

    add: catppuccinテーマを追加

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,7 +11,30 @@ require('tokyonight').setup({
   }
 })
 
+-- Catppuccin
+MiniDeps.add({ source = 'catppuccin/nvim', name = 'catppuccin' })
+require('catppuccin').setup({
+  flavor = 'auto',
+  background = {
+    light = 'latte',
+    dark = 'mocha'
+  },
+  transparent_background = false,
+  default_integrations = true,
+  integrations = {
+    notify = true,
+    mini = {
+      enable = true
+    }
+  }
+})
+
+-- Edge
+MiniDeps.add({ source = 'sainnhe/edge', name = 'edge' })
+
 MiniDeps.now(function()
-  vim.cmd.colorscheme('tokyonight')
+  -- vim.g.edge_style = 'default'
+  -- vim.g.edge_better_performance = 1
+  vim.cmd.colorscheme('catppuccin')
 end)
 
